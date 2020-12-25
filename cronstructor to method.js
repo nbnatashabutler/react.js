cb32da09a44f4e6a75e0ea51ccaf3d7713514ac3
Natasha Butler <nbnatashabutler@gmail.com>
2020-12-24 19:10:57 -0500

    Create cronstructor to method.js

---
+++ cronstructor to method.js
@@ -0,0 +1,14 @@
+//if rendering in HTML directly insert the constructor method and function in to script tags.
+
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+  
+  present() {
+    return 'I have a ' + this.brand;
+  }
+}
+
+mycar = new Car("Ford");
+mycar.present();
