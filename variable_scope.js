2e028b9fb482abea50105bc6be9261288585cb99
Natasha Butler <nbnatashabutler@gmail.com>
2020-12-24 20:28:18 -0500

    Create variable_scope.js

---
+++ variable_scope.js
@@ -0,0 +1,13 @@
+
+//outside of a function var belongs to the global object
+//inside of a function var belongs to function it is in initially
+//inside a for loop it is available for other objects 
+var x = 5.6 
+
+//the variable is only available where it is defined initially
+
+let x = 5.6
+
+//const has a block scope just as let does and var when var is in a block
+const x = 5.6
+
